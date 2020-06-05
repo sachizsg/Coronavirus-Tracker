a39cc984489ce0da5db6e8dd096e0a2837936335
sachizsg <wrsachithra@gmail.com>
2020-06-05 21:48:56 +0530

    Create coronavirus tracking application

---
+++ coronavirus_tracker/src/main/java/coronavirus_tracker/controller/HomeController.java
@@ -0,0 +1,32 @@
+package coronavirus_tracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import coronavirus_tracker.model.LocationStats;
+import coronavirus_tracker.services.CoronaVirusDataService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	CoronaVirusDataService coronaVirusDataService;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		
+	List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+    int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotal()).sum();
+    int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+    model.addAttribute("locationStats", allStats);
+    model.addAttribute("totalReportedCases", totalReportedCases);
+    model.addAttribute("totalNewCases", totalNewCases);
+    
+    return "home";
+	}
+
+}
